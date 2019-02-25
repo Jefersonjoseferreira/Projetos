b0e8194befbfe6d911e708dfbfe13021fbd60fcd
Jefersonjoseferreira <47761963+Jefersonjoseferreira@users.noreply.github.com>
2019-02-25 20:07:04 -0300

    Add files via upload

---
+++ projeto 2.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+//	system("cls");
+	
+	int A=0;
+	
+	printf("Informe um valor A inteiro: \n");
+	
+	scanf("%i",&A);
+	
+	printf("O valor de A eh: %i \n",A);
+	
+//	system("pause"); 
+	return 1;
+}
