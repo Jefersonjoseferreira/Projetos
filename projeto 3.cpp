73675ea70ff3b5a75b094a22a48200155b44b6fe
Jefersonjoseferreira <47761963+Jefersonjoseferreira@users.noreply.github.com>
2019-02-25 21:46:12 -0300

    Add files via upload

---
+++ projeto 3.cpp
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int A=0;
+	int B=0;
+	
+	printf("Informe um valor A inteiro: \n");
+	
+	scanf("%i",&A);
+	
+	printf("O valor de A eh: %i \n",A);
+	
+	printf("Informe um valor B inteiro: \n");
+	
+	scanf("%i",&B);
+	
+	printf("O valor de B eh: %i \n",B);
+	
+	printf("A soma dos valores e: %i \n",A+B);
+	printf("A subtração dos valores e: %i \n",A-B);
+	printf("A multiplicação dos valores e: %i \n",A*B);
+	printf("A divisão dos valores e: %i \n",A/B);
+	
+	
+
+	return 1;
+}
