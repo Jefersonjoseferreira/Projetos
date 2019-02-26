cdabec3695c43108bca850433cbffb8b6984e7af
Jefersonjoseferreira <47761963+Jefersonjoseferreira@users.noreply.github.com>
2019-02-25 22:06:03 -0300

    Add files via upload

---
+++ Projeto 1.c
@@ -0,0 +1,27 @@
+// codigo em linguagem c
+
+#include<stdio.h>
+
+int main()
+{
+		
+	int A=2;
+	int B=4;
+	
+	if(A>B)
+	{
+		printf("A é maior que B! \n");
+	}
+	else
+	{
+		if(A<B)
+		{
+		printf("B é maior que A! \n");
+		}
+		else
+		{
+		printf("B é igual a A! \n");
+		}
+	}
+return 1;
+}
